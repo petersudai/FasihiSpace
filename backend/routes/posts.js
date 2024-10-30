ad199ded9c799a4167977d8f49e1f7e7401f0e6e
petersudai <81692851+petersudai@users.noreply.github.com>
2024-10-30 21:27:47 +0300

    Cloudinary setup

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { createPost, getPosts, getPostByID, editPost, deletePost, searchPosts } = require('../controllers/postController');
 const auth = require('../middleware/auth');
 const router = express.Router();
+const upload = require('../config/cloudinary')
 
 // Create a new blog post
 router.post('/', auth, createPost);
@@ -20,5 +21,11 @@ router.delete('/:id', auth, deletePost);
 
 // Search and filter posts
 router.get('/search', searchPosts);
+
+// Upload images to Cloudinary
+router.post('/upload', upload.single('image'), (req, res) => {
+    console.log('File uploaded to Cloudinary:', req.file);
+    res.json({ url: req.file.path });
+  });
 
 module.exports = router;
